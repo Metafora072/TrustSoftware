790903b887a01ad591e30c1b98a27a000e361ac4
Metafora <2430093500@qq.com>
2023-11-29 10:40:50 +0800

    添加三种折扣类并封装其方法

--- Huawei_cpp_training_1/include/PriceCalculator.h
+++ Huawei_cpp_training_1/include/PriceCalculator.h
@@ -15,4 +15,42 @@ namespace PriceCalc
     public:
         double AcceptCash(const DiscountType discountType, const double money) const noexcept;
     };
+
+    class Normal final
+    {
+    public:
+        double AcceptCash(const double money) const noexcept{
+            return money;
+        }
+        virtual ~Normal();
+    };
+    class PercentOff final
+    {
+    private:
+        double discountRate = 1;
+    public:
+        explicit PercentOff(double discountRate){
+            this->discountRate = discountRate;
+        }
+        virtual ~PercentOff();
+
+        double AcceptCash(const double money) const noexcept{
+            return money * discountRate;
+        }
+    };
+    class CashBack final
+    {
+    private:
+        double threshold = 100.0;
+        double cashback = 20.0;
+    public:
+        explicit CashBack(double threshold,double cashback){
+            this->threshold = threshold;
+            this->cashback = cashback;
+        }
+        virtual ~CashBack();
+        double AcceptCash(const double money) const noexcept {
+            return money - floor(money / threshold ) * cashback;
+        }
+    };
 }  // namespace PriceCalc
